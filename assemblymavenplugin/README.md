f7b8bd37df8cfcc8c80642c5e0af87ad3b3fbfb9
Nagaraju <nagaraju_g@yaskawa.in>
2019-10-19 14:15:56 +0530

    Deleted assembly app.

--- assemblymavenplugin/README.md
+++
@@ -1,7 +0,0 @@
-# Assembly maven plugin.
-
-Jar creation process.
-
-1. mvn clean
-2. mvn complie
-3. mvn package
--- assemblymavenplugin/src/test/java/com/theprogrammersbook/ApplicationTest.java
+++
@@ -1,7 +0,0 @@
-package com.theprogrammersbook;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ApplicationTest {
-
-}
