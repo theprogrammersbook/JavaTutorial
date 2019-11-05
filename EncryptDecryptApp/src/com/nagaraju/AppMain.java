40ec59f3ef196f50b66b8fe6b99f2762c341d56f
Nagaraju <nagaraju_g@yaskawa.in>
2019-11-05 14:13:09 +0530

    modified code.

--- EncryptDecryptApp/src/com/nagaraju/AppMain.java
+++ EncryptDecryptApp/src/com/nagaraju/AppMain.java
@@ -9,6 +9,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Enumeration;
 
 import javax.crypto.Cipher;
@@ -18,7 +19,9 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AppMain {
   public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException, InvalidAlgorithmParameterException {
-	  String originalContent = "{\"MACAddress\":\"54-E1-AD-EF-5C-6E\",\"Version\":\"1.2\",\"ExpiryDate\":\"2019-03-23 12:23:0\"}";
+	  long time = new Date("01/01/2020").getTime()/1000;
+	  String currentTime = String.valueOf(time+1500);
+	  String originalContent = "{\"MACAddress\":\"54-E1-AD-EF-5C-6E\",\"Version\":\"20\",\"ExpiryDate\":"+currentTime+"}";
 	  String key = "ROBOTIOTA1$%#$%#";
 	  getMacAddress("Good");
 	  System.out.println(key.length());
@@ -27,11 +30,11 @@ public class AppMain {
       SecretKey secretKey =  new SecretKeySpec(key.getBytes(), "AES");
 
       FileEncrypterDecrypter fileEncrypterDecrypter = new FileEncrypterDecrypter(secretKey, "AES/CBC/PKCS5Padding");
-      fileEncrypterDecrypter.encrypt(originalContent, "baz.enc");
+      fileEncrypterDecrypter.encrypt(originalContent, "robotIOTLicense.enc");
 
-      String decryptedContent = fileEncrypterDecrypter.decrypt("baz.enc");
+     // String decryptedContent = fileEncrypterDecrypter.decrypt("baz.enc");
 		/* assertThat(decryptedContent, is(originalContent)); */
-        System.out.println(decryptedContent);
+      //  System.out.println(decryptedContent);
       //new File("baz.enc").delete(); // cleanup  
 }
   public static boolean getMacAddress(String macAddress) {
